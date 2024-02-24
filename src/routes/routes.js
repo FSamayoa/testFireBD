47ef8a5360b31e5dde15979c487114838f325197
Samayoa <fsamayoal@gmail.com>
2024-02-23 18:07:01 -0600

    hasta aqui funciona bien

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,38 @@
 const { Router } = require('express');
-const { log } = require('handlebars');
 const routes = Router()
+const admin = require('firebase-admin')
+require('dotenv').config();
+
+//$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\jasam\OneDrive\Escritorio\testbackfire\server\fireback-111-firebase-adminsdk-190pm-51d865ee7e.json"
+var serviceAccount = require('../../fireback-111-firebase-adminsdk-190pm-51d865ee7e.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.DB_URL
+})
+
+const db = admin.database()
 
 routes.get('/',(req, res) =>{
+    db.ref('contactos').once('value',(snapshot)=>{
+        const data = snapshot.val()
+        res.render('index', {contactos: data})
+    })
     console.log("Get en index");
-   
-    res.render('index')
 })
 
 routes.post('/new-contact', (req,res)=>{
     console.log(req.body);
-    res.send('contacto nuevo')
+    const newContact = {
+        name: req.body.name,
+        phone: req.body.phone
+    }
+    db.ref('contactos').push(newContact)
+    res.redirect('/')
+})
+
+routes.get('/borrar/:id', (req,res)=>{
+    db.ref('contactos/' + req.params.id).remove()
+    res.redirect('/')
 })
 
 module.exports = routes;
