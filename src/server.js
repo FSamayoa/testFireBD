99ed1b5c48165e745a6ca6bd91bf4230d0b5fb8b
Samayoa <fsamayoal@gmail.com>
2024-02-29 12:43:59 -0600

    cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require ('express');
+const cors = require('cors');
 const morgan = require ('morgan');
 const path = require('path');
 
 const server = express();
 
 
+server.use(cors());
 
 //*middlewares
 server.use(morgan('dev'));
