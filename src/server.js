189ff7af20083877f30b14d148e8fd8be0af6077
Samayoa <fsamayoal@gmail.com>
2024-03-01 14:15:26 -0600

    safas

--- src/server.js
+++ src/server.js
@@ -1,26 +1,40 @@
-const express = require ('express');
-const cors = require('cors');
-const morgan = require ('morgan');
-const path = require('path');
+const express = require("express");
+const routes = require("../src/routes/routes")
 
-const server = express();
+server = express()
 
 
-server.use(cors({
-    origin: '*' 
-  }));
-
-//*middlewares
-server.use(morgan('dev'));
-    server.use(express.urlencoded({extended: true}));
-server.use(express.json());
-
-//*routes
-
-server.use(require('./routes/routes'))
-
-//*static Files
-server.use(express.static(path.join(__dirname, '../server')));
+server.use("/api",routes)
 
 
 module.exports = server
+
+
+
+
+// const express = require ('express');
+// const cors = require('cors');
+// const morgan = require ('morgan');
+// const path = require('path');
+
+// const server = express();
+
+
+// server.use(cors({
+//     origin: '*' 
+//   }));
+
+// //*middlewares
+// server.use(morgan('dev'));
+//     server.use(express.urlencoded({extended: true}));
+// server.use(express.json());
+
+// //*routes
+
+// server.use(require('./routes/routes'))
+
+// //*static Files
+// server.use(express.static(path.join(__dirname, '../server')));
+
+
+// module.exports = server
